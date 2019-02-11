421d5e3a32ec5f8ef8c0a8268fa0e447a89a34cd
salman <salman.hanif917@gmail.com>
2019-02-12 06:13:39 +0800

    Initial Commit

--- app/src/main/java/com/example/salma/myapplication/ContactActivity.java
+++ app/src/main/java/com/example/salma/myapplication/ContactActivity.java
@@ -6,15 +6,20 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class ContactActivity extends AppCompatActivity {
 
+    EditText Name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
+
 
 
         assert getSupportActionBar() != null;   //null check
